c379de769a7174f75d3ec50b3cbc6f3c1ff299e3
Gáspár József Dániel <gaspar.jozsef.daniel@gmail.com>
2020-08-11 14:35:29 +0200

    Add Swashbuckle Swagger API

--- api/WebAPI/Startup.cs
+++ api/WebAPI/Startup.cs
@@ -3,7 +3,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using ShareGithub;
+using System;
+using System.Reflection;
+using System.IO;
+
 
 namespace WebAPI
 {
@@ -28,15 +33,31 @@ namespace WebAPI
                        .AllowAnyMethod()
                        .AllowAnyHeader();
             }));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v0", new OpenApiInfo
+                {
+                    Version = "v0",
+                    Title = "Share-Github API",
+                    Description = "API for the Share-Github project"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+        { 
+            app.UseSwagger();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v0/swagger.json", "Share-Github API v0");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseCors("ANY");
 
