a62153ad64d095becc1d84f887e460a0ca7c30ec
Gáspár József Dániel <gaspar.jozsef.daniel@gmail.com>
2020-08-15 21:51:35 +0200

    Fix BranchSelector popping behaviour

--- web/src/components/BranchSelector.tsx
+++ web/src/components/BranchSelector.tsx
@@ -27,10 +27,13 @@ export default class BranchSelector extends React.Component<IProps, IState> {
         super(props);
     }
     componentDidMount() {
+        this.state.current = this.props.current;
+        // Assume the branch we're trying to display is a valid branch to avoid popping
+        this.state.branches = [this.props.current];
+        this.setState(this.state);
         API.getBranches(this.props.user, this.props.repo, this.state.cancelToken)
         .then((res) => {
             this.state.branches = res;
-            this.state.current = this.props.current;
             this.setState(this.state);
         })
     }
@@ -49,7 +52,7 @@ export default class BranchSelector extends React.Component<IProps, IState> {
                       search
                       selection
                       key={this.state.current}
-                      value={this.props.current}
+                      value={this.state.current}
                       onChange={
                         (event, props) => {
                             if(props.value != undefined) {
