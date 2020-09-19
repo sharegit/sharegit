07edef13e87f36092d049f2b287c8cc9e6add28b
Horváth Martin <horvathmartin@protonmail.com>
2020-09-19 13:12:57 +0200

    Replace semantic-ui in SharedWithMe.

--- web/src/app/SharedWithMe/index.tsx
+++ web/src/app/SharedWithMe/index.tsx
@@ -2,10 +2,14 @@ import { Token, TokenRepo } from 'models/Tokens';
 import React from 'react';
 import { RouteComponentProps } from 'react-router';
 import { Link } from 'react-router-dom';
-import { Button, List } from 'semantic-ui-react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import styles from './style.scss';
 import ContentPanel from 'components/ContentPanel';
 import LocalStorageDictionary from 'util/LocalStorageDictionary';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import { Button } from '@material-ui/core';
 
 export interface IProps extends RouteComponentProps<any> {
 }
@@ -23,12 +27,12 @@ export default class SharedWithMe extends React.Component<IProps, IState> {
     }
     componentDidMount() {
         const allTokens = new LocalStorageDictionary<Token>('alltokens');
-        
+
         this.state.tokens = allTokens.getAll().filter(x => {
             const d = x.tokenExp == undefined ? undefined : new Date(x.tokenExp);
             const canStay = d == undefined || d.getTime() > new Date().getTime();
 
-            if(!canStay)
+            if (!canStay)
                 allTokens.remove(x.token);
 
             return canStay;
@@ -37,8 +41,8 @@ export default class SharedWithMe extends React.Component<IProps, IState> {
         this.setState(this.state);
     }
     forget(token: string) {
-        const toForget = this.state.tokens.findIndex(x=>x.token == token);
-        if(toForget > -1) {
+        const toForget = this.state.tokens.findIndex(x => x.token == token);
+        if (toForget > -1) {
             this.state.tokens.splice(toForget, 1);
 
             const tokens = JSON.stringify(this.state.tokens);
@@ -48,7 +52,7 @@ export default class SharedWithMe extends React.Component<IProps, IState> {
         }
     }
     renderTokenHeader(token: Token): string {
-        if(!!token.author) {
+        if (!!token.author) {
             return `${token.author}'s [${!!token.customName ? token.customName : token.token}]`
         } else {
             return `[${token.token}]`
@@ -59,42 +63,34 @@ export default class SharedWithMe extends React.Component<IProps, IState> {
             <ContentPanel background='light'>
                 <div id={styles.sharedWithMe}>
                     <h2>Tokens shared with you</h2>
-                    {this.state.tokens.length == 0 ?
-                        <p>You have no tokens yet!</p>
-                    : null}
-                    <List divided relaxed>
+                    {this.state.tokens.length == 0 ? <p>You have no tokens yet!</p> : null}
+                    <List>
                         {
-                            this.state.tokens
-                                .map((token : Token) =>
-                                <List.Item key={token.token}>
-                                    <List.Content>
-                                        <List.Header>
-                                            <Link to={`/share/${token.token}`}>{this.renderTokenHeader(token)}</Link>
-                                        </List.Header>
-                                        <List.Description id={styles.list}>
-                                            {token.tokenExp != undefined ? 
-                                                'Expires on: ' + token.tokenExp : ''}
-                                            {token.repositories.length == 0 ?
-                                            <p>Not yet visited</p>
-                                        :
-                                            token.repositories.map((r:TokenRepo) => (
-                                                <span key={`${r.provider}/${r.owner}/${r.name}`}>
-                                                    {`\[${r.provider}\]/${r.owner}/${r.name}`}
-                                                </span>
-                                            ))}
-                                        </List.Description>
-                                        <Button secondary onClick={(e, d) => {
-                                            this.forget(token.token)
-                                        }}>Forget</Button>
-
-                                        <div className="clear"></div>
-                                    </List.Content>
-                                </List.Item>
-                                )
+                            this.state.tokens.map((token: Token) =>
+                                [
+                                    <ListItem button key={token.token} component={Link} to={`/share/${token.token}`} >
+                                        <ListItemText primary={this.renderTokenHeader(token)} secondary={token.tokenExp != undefined ? 'Expires on: ' + token.tokenExp : ''} />
+                                        <List disablePadding dense >
+                                            {
+                                                token.repositories.length == 0 ?
+                                                    <ListItem component={ListItemText} primary="Not yet visited" /> :
+                                                    token.repositories.map((r: TokenRepo) => (
+                                                        <ListItem
+                                                            key={`${r.provider}/${r.owner}/${r.name}`}
+                                                            component={ListItemText}
+                                                            primary={`\[${r.provider}\]/${r.owner}/${r.name}`} />
+                                                    ))
+                                            }
+                                        </List>
+                                        <ListItemSecondaryAction>
+                                            <Button variant="contained" color="primary" onClick={() => { this.forget(token.token) }}>Forget</Button>
+                                        </ListItemSecondaryAction>
+                                    </ListItem>
+                                ])
                         }
                     </List>
                 </div>
-            </ContentPanel>
+            </ContentPanel >
         )
     }
 }
