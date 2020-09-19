3b833d2821fa3e9544d17b1d77c4791a0fb5fd7d
Horváth Martin <horvathmartin@protonmail.com>
2020-09-19 13:13:21 +0200

    Replace semantic-ui icon in RepositoryCard.

--- web/src/app/SharedLanding/RepositoryCard/index.tsx
+++ web/src/app/SharedLanding/RepositoryCard/index.tsx
@@ -1,3 +1,4 @@
+import { GetApp } from '@material-ui/icons';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Icon, List } from 'semantic-ui-react';
@@ -22,15 +23,15 @@ export default class RepositoryCard extends React.Component<IProps, IState> {
                 <List.Icon name={this.props.provider} size='large' verticalAlign='middle' />
                 <List.Content>
                     <List.Header>
-                        {this.props.link.startsWith('https://') ? 
-                        <a href={this.props.link} target="_blank">
-                            {this.props.name}
-                        </a>
-                    :
-                        <Link target={this.props.target} to={this.props.link}>
-                            {this.props.name}
-                        </Link>}
-                        {this.props.downloadable ? <Icon name='download'></Icon> : null}
+                        {this.props.link.startsWith('https://') ?
+                            <a href={this.props.link} target="_blank">
+                                {this.props.name}
+                            </a>
+                            :
+                            <Link target={this.props.target} to={this.props.link}>
+                                {this.props.name}
+                            </Link>}
+                        {this.props.downloadable ? <GetApp /> : null}
                     </List.Header>
                     <List.Description>{this.props.description}</List.Description>
                     {this.props.children}
