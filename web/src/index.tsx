ad7fc96f7666ff3d084706b1384d944e8fa43d21
Gáspár József Dániel <gaspar.jozsef.daniel@gmail.com>
2020-09-28 19:13:56 +0200

    Remove semantic-ui from the project

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
-import 'semantic-ui-css/semantic.min.css';
 import App from './app';
 import API from './models/API';
 
